5586d27585e2c0024064c7ad6d824712bbe1ccfe
Vlad Balabash <vladb951@gmail.com>
2019-01-22 23:49:12 +0200

    added english

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import Vue from 'vue';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue';
+import locale from 'element-ui/lib/locale/lang/en';
 
-Vue.use(ElementUI);
+Vue.use(ElementUI, { locale });
 
 Vue.config.productionTip = false;
 
