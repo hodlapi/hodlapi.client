87b0c7344662169386c48553d57a595b6eb3024d
Kirill Museychuk <kmuseychuk@lohika.com>
2019-02-06 19:44:38 +0200

    New designs

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,6 @@
 <template>
   <div id="app">
-    <el-row type="flex" :justify="'center'" :glutter="20">
-      <el-col :span="12" :xs="24">
-        <request-form></request-form>
-      </el-col>
-    </el-row>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import RequestForm from "./components/RequestForm.vue";
-
-export default {
-  name: "app",
-  components: {
-    RequestForm
-  }
-};
-</script>
 
--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
 import Vue from 'vue';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import './core/styles/index.scss';
 import App from './App.vue';
 import locale from 'element-ui/lib/locale/lang/en';
+import router from './router';
+import store from './store';
 
 Vue.use(ElementUI, { locale });
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app');
