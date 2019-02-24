6aa890972a70b8da16720f48c7c327e13feb3873
Vlad Balabash <vladb951@gmail.com>
2019-02-24 20:13:07 +0200

    added 401 error handler

--- src/App.vue
+++ src/App.vue
@@ -4,3 +4,16 @@
   </div>
 </template>
 
+<script>
+import { EventBus } from "./core/lib";
+
+export default {
+  mounted() {
+    EventBus.$on("error.invalidToken", () => {
+      this.$store
+        .dispatch("user/logout")
+        .then(() => this.$router.replace("/login"));
+    });
+  }
+};
+</script>
--- src/core/lib/index.js
+++ src/core/lib/index.js
@@ -1,5 +1,7 @@
 import api from './api';
+import EventBus from './eventBus';
 
 export {
-    api
+    api,
+    EventBus
 };
